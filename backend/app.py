0064ec7db93f6143412f47e771cbdfdfa522ed50
kalil nobrega <kalilnobrega@MacBook-Air-de-kalil.local>
2025-11-17 08:23:56 -0300

    rota /load

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from Simulador import Simulator
+
+app = Flask(__name__)
+CORS(app)
+
+vm = Simulator()
+
+@app.route("/load", methods=["POST"])
+def load_program():
+    
+    try:
+        data = request.get_json()
+        code = data.get("code", "")
+
+        if not code.strip():
+            return jsonify({"error": "Nenhum código recebido"}), 400
+
+        vm.load_program_from_text(code)
+
+        return jsonify({"status": "program loaded", "instructions": len(vm.program)})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
